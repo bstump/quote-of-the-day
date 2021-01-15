caad4d08505b7cc6ffe1cf7409e679f930a1ccdd
Bobbie Stump <bobbie.stump@gmail.com>
2021-01-15 08:54:43 -0500

    feat: vue front-end setup

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,31 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
 require('./bootstrap');
+window.Vue = require('vue').default;
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+import QuoteOfTheDay from '@/js/views/QuoteOfTheDay'
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: QuoteOfTheDay,
+        },
+    ],
+});
+
+const app = new Vue({
+    el: '#app',
+    components: { QuoteOfTheDay },
+    router
+});
---
+++ resources/js/views/QuoteOfTheDay.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="row home">
+        <div class="col-sm-12">
+            Quote of the Day
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    created() {
+        alert('hello')
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
